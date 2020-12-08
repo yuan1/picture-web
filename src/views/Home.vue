733504e078da78a61f7a1d8fefcb953c7c0c1fac
李明元 <limingyuan@czkj1010.com>
2020-12-08 15:49:25 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="fav-list">
+      <div
+        v-for="(pic, index) in pictureData"
+        :key="index"
+        :style="{
+          flexBasis: (pic.width * 240) / pic.height + 'px',
+          flexGrow: (pic.width * 100) / pic.height
+        }"
+      >
+        <i :style="{ paddingBottom: (pic.height / pic.width) * 100 + '%' }"></i>
+        <img :src="pic.watermarkImage" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      pictureData: require("@/assets/data/cats.json")
+    };
+  },
+  created() {
+    console.log(this.pictureData);
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  .fav-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 2px;
+    text-align: left;
+    // 最后一行展示形式，可注释掉查看另一种形式
+    &::after {
+      content: "";
+      flex-grow: 999999999;
+      order: 999999999;
+    }
+    & > div {
+      margin: 2px;
+      background-color: violet;
+      position: relative;
+      // 加载占位符
+      & > i {
+        display: block;
+        background-color: lightblue;
+        /* display: none; */
+      }
+      & > img {
+        position: absolute;
+        vertical-align: bottom;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+  }
+}
+</style>
